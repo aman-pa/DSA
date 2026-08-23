356604b28051efe74aca9d4b4af78f789ba2b78f
Aman Pandey <amanpandey10a3@gmail.com>
2026-08-23 10:56:26 +0530

    Time: 0 ms (100%), Space: 65 MB (30.96%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -11,13 +11,13 @@
  */
 class Solution {
 public:
-int s=0;
+    int s=0;
     int rangeSumBST(TreeNode* root, int low, int high) {
         if(root==NULL)return 0;
         if(root->val>=low&&root->val<=high)s+=root->val;
         rangeSumBST(root->left,low,high);
         rangeSumBST(root->right,low,high);
+
         return s;
-   
     }
 };
