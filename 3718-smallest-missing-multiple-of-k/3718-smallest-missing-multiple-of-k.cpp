48dcd7dfcf97a97821c83b245ee2578983b5f3d3
Aman Pandey <amanpandey10a3@gmail.com>
2026-08-25 10:10:57 +0530

    Time: 2 ms (42.52%), Space: 25.1 MB (45.06%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
         int mx=*max_element(nums.begin(),nums.end());
-        set<int>s;
+        unordered_set<int>s;
         int p=nums.size();
         for(int i=0;i<p;i++){
             s.insert(nums[i]);
