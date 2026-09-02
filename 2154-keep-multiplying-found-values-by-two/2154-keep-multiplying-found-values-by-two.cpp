0a030bc83f31c416a3d276e242baf5d41faad0d8
Aman Pandey <amanpandey10a3@gmail.com>
2026-09-02 19:31:24 +0530

    Time: 3 ms (42.14%), Space: 14.4 MB (41.65%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    bool search(vector<int>&nums,int n,int  &original,long long int &k){
-         for(int i=0;i<n;i++){
-            if(nums[i]==original){k*=2;
-            original=k;
-            return true;}
-        }
-        return false;
-    }
     int findFinalValue(vector<int>& nums, int original) {
         int n=nums.size();
-        long long int k=original;
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int x:nums)pq.push(x);
         while(true){
-            bool res=search(nums,n,original,k);
-            if(res==true)continue;
-            else return k;
+            if(pq.size()==0)return original;
+            if(pq.top()==original)original*=2;
+            else if(pq.top()<original){
+                pq.pop();
+            }
+            else return original;
         }
-        return k;
-       
+        return original;
     }
 };
