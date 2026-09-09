fc0b7620d170fbabb67597f6481899278d54249d
Aman Pandey <amanpandey10a3@gmail.com>
2026-09-09 11:19:39 +0530

    Time: 0 ms (100%), Space: 9 MB (57.33%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -2,19 +2,24 @@ class Solution {
 public:
     long long countCommas(long long n) {
         if(n<1000)return 0;
-        string st=to_string(n);
-        int s=st.size();
-        if(s<3)return 0;
-        if(s>=4&&s<=6){
-            return n-999;
-        }
-        if(s>=7&&s<=9){
-            return n-999999+n-999;
-        }
-        if(s>=10&&s<=12)return n-999999999+n-999999+n-999;
-       if(s>=13&&s<=15)return n-999999999999+n-999999999+n-999999+n-999;
-       if(s>15)return n-999999999999999+n-999999999999+n-999999999+n-999999+n-999;
-       return 0;
+    //     string st=to_string(n);
+    //     int s=st.size();
+    //     if(s<3)return 0;
+    //     if(s>=4&&s<=6){
+    //         return n-999;
+    //     }
+    //     if(s>=7&&s<=9){
+    //         return n-999999+n-999;
+    //     }
+    //     if(s>=10&&s<=12)return n-999999999+n-999999+n-999;
+    //    if(s>=13&&s<=15)return n-999999999999+n-999999999+n-999999+n-999;
+    //    if(s>15)return n-999999999999999+n-999999999999+n-999999999+n-999999+n-999;
+    //    return 0;
+    long long ans=0;
+    for(long long i=1000;i<=n;i*=1000){
+        ans+=n-(long long)i+1;
+    }
+    return ans;
         
     }
 };
