fafa9458f1b52dd52116fca7822cdeeccb69b6e2
Aman Pandey <amanpandey10a3@gmail.com>
2026-09-24 09:59:45 +0530

    Time: 0 ms (100%), Space: 52.4 MB (25.92%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int m=0;
+        for(int i=0;i<n;i++){
+            if(i>m)return false;
+            m=max(m,i+nums[i]);
+        }
+        return true;
+    }
+};
