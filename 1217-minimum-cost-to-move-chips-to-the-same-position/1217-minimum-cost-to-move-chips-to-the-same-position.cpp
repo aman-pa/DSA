2edfb2f10545a3ad218fb1344bf1ba35bd585c2a
Aman Pandey <amanpandey10a3@gmail.com>
2026-09-25 12:22:13 +0530

    Time: 0 ms (100%), Space: 9.8 MB (70.4%) - LeetHub

--- 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.cpp
+++ 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.cpp
@@ -2,22 +2,28 @@ class Solution {
 public:
     int minCostToMoveChips(vector<int>& position) {
         int n=position.size();
-        int m=INT_MAX;
-        for(int i=0;i<n;i++){
-            int c=0;
-            int k=position[i];
-           for(int j=0;j<n;j++){
-                int l=position[j];
-                if((abs(l-k))%2==0){
-                   continue;
-                }
-                if((abs(l-k))%2!=0){
-                    c+=1;
-                }
+        // int m=INT_MAX;
+        // for(int i=0;i<n;i++){
+        //     int c=0;
+        //     int k=position[i];
+        //    for(int j=0;j<n;j++){
+        //         int l=position[j];
+        //         if((abs(l-k))%2==0){
+        //            continue;
+        //         }
+        //         if((abs(l-k))%2!=0){
+        //             c+=1;
+        //         }
                 
+        // }
+        //                 m=min(m,c);
+        // }
+        // return m;
+        int o=0,e=0;
+        for(int x:position){
+            if(x%2==0)e+=1;
+            else o+=1;
         }
-                        m=min(m,c);
-        }
-        return m;
+        return min(o,e);
     }
 };
