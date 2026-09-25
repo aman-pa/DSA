9ab46ffa743835f3190d0c1fcae1e2ec540ecaff
Aman Pandey <amanpandey10a3@gmail.com>
2026-09-25 18:41:30 +0530

    Time: 0 ms (100%), Space: 12.6 MB (80.55%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.cpp
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.cpp
@@ -4,31 +4,32 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
 
-        int mn = INT_MAX;
+        int p = INT_MAX;
 
-        for (int x : nums) {
-            mn = min(mn, abs(x));
+        for(int i = 0; i < n; i++) {
+            p = min(p, abs(nums[i]));
         }
 
-        for (int i = 0; i < n && k > 0; i++) {
-            if (nums[i] < 0) {
+        for(int i = 0; i < n; i++) {
+            if(k > 0 && nums[i] < 0) {
                 nums[i] *= -1;
                 k--;
-            } else {
+            }
+            else {
                 break;
             }
         }
 
-        long long sum = 0;
+        int s = 0;
 
-        for (int x : nums) {
-            sum += x;
+        for(int x : nums) {
+            s += x;
         }
 
-        if (k % 2 != 0) {
-            sum -= 2LL * mn;
+        if(k % 2 != 0) {
+            s -= 2 * p;
         }
 
-        return sum;
+        return s;
     }
 };
